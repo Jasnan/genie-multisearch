0c4def509d0e5b91171a76a1b134ff84c8bab0dc
Jasnan TP <tp.jasnan@hotmail.com>
2016-01-11 01:33:45 +0530

    Genie new features: show/hide a language from menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,226 +1,331 @@
-'use strict';
-angular.module('credise', ['ngStorage','ui.router','ngSanitize'])
-	.controller('popupCtrl', ['$scope','$localStorage','Languages', function($scope,$localStorage,Languages){
+"use strict";
+angular.module("credise", ["ngStorage", "ui.bootstrap", "ui.router", "ngSanitize"])
+  .controller("popupCtrl", ["$scope", "$localStorage", "Languages", function($scope, $localStorage, Languages) {
 
-		$scope.searchTerm = "";
-		$scope.nativeLang = Languages.native();
-		$scope.langOptions = Languages.options();
-		$scope.searchLang = Languages.searchLang();
+    $scope.searchTerm = "";
+    $scope.nativeLang = Languages.myNative();
+    $scope.langMenu = Languages.langMenu();
+    $scope.searchLang = Languages.searchLang();
 
-		console.log($scope.langOptions)
-		$scope.search = function(form){
-			$scope.submitted = true;
-			if(form.$valid){
-				if ($scope.searchTerm){
-					var alltabs = Languages.getTabs($scope.nativeLang + $scope.searchLang)
-					console.log(alltabs)
-					var alltabs =
-						alltabs
-						.filter(function(tab){
-							return (tab.url.indexOf("[search]") > -1);
-						})
-						.map(function(tab){
-							var newtaburl = tab.url.replace("[search]",$scope.searchTerm);
-							if(tab.translate){
-								var trurl = Languages.getTranslator($scope.searchLang);
-								var newtaburlencoded = encodeURIComponent(newtaburl).replace(/'/g,"%27").replace(/"/g,"%22");
-								newtaburl = trurl.replace("[translate]",newtaburlencoded);
-							}
+    $scope.search = function(form) {
+      $scope.submitted = true;
+      if (form.$valid) {
+        if ($scope.searchTerm) {
+          var alltabs = Languages.getTabs($scope.nativeLang + $scope.searchLang);
+          alltabs =
+            alltabs
+            .filter(function(tab) {
+              return (tab.url.indexOf("[search]") > -1);
+            })
+            .map(function(tab) {
+              var newtaburl = tab.url.replace("[search]", $scope.searchTerm);
+              if (tab.translate) {
+                var trurl = Languages.getTranslator($scope.nativeLang + $scope.searchLang);
+                var newtaburlencoded = encodeURIComponent(newtaburl).replace(/'/g, "%27").replace(/"/g, "%22");
+                newtaburl = trurl.replace("[translate]", newtaburlencoded);
+              }
 
-							return newtaburl
-						})
+              return newtaburl;
+            });
 
-					createTabs(alltabs)
-				} 
-			}
-		}
+          createTabs(alltabs);
+        }
+      }
+    };
 
-		$scope.openSettings = function(){
-			chrome.tabs.create({'url': "/views/options.html" } )
-		}
-	}])
+    $scope.openSettings = function() {
+      chrome.tabs.create({
+        'url': "/views/options.html"
+      });
+    };
+  }])
 
-	.controller('backgroundCtrl', ['$scope','$localStorage','Languages', function($scope,$localStorage,Languages){
+.controller('backgroundCtrl', ['$scope', '$localStorage', 'Languages', function($scope, $localStorage, Languages) {
 
-		$scope.searchTerm = "";
-		$scope.nativeLang = Languages.native();
-		$scope.langOptions = Languages.options();
-		$scope.searchLang = Languages.searchLang();
+  $scope.searchTerm = "";
+  $scope.nativeLang = Languages.myNative();
+  $scope.langMenu = Languages.langMenu();
+  $scope.searchLang = Languages.searchLang();
 
-		console.log($scope.langOptions)
-		$scope.search = function(form){
-			// $scope.submitted = true;
-			// if(form.$valid){
-				if ($scope.searchTerm){
-					var alltabs = Languages.getTabs($scope.nativeLang + $scope.searchLang)
-					console.log(alltabs)
-					var alltabs =
-						alltabs
-						.filter(function(tab){
-							return (tab.url.indexOf("[search]") > -1);
-						})
-						.map(function(tab){
-							var newtaburl = tab.url.replace("[search]",$scope.searchTerm);
-							if(tab.translate){
-								var trurl = Languages.getTranslator($scope.searchLang);
-								var newtaburlencoded = encodeURIComponent(newtaburl).replace(/'/g,"%27").replace(/"/g,"%22");
-								newtaburl = trurl.replace("[translate]",newtaburlencoded);
-							}
+  $scope.search = function(form) {
+    // $scope.submitted = true;
+    // if(form.$valid){
+    if ($scope.searchTerm) {
+      var alltabs = Languages.getTabs($scope.nativeLang + $scope.searchLang)
+      alltabs =
+        alltabs
+        .filter(function(tab) {
+          return (tab.url.indexOf("[search]") > -1);
+        })
+        .map(function(tab) {
+          var newtaburl = tab.url.replace("[search]", $scope.searchTerm);
+          if (tab.translate) {
+            var trurl = Languages.getTranslator($scope.nativeLang + $scope.searchLang);
+            var newtaburlencoded = encodeURIComponent(newtaburl).replace(/'/g, "%27").replace(/"/g, "%22");
+            newtaburl = trurl.replace("[translate]", newtaburlencoded);
+          }
 
-							return newtaburl
-						})
+          return newtaburl;
+        });
 
-					createTabs(alltabs)
-				} 
-			// }
-		}
+      createTabs(alltabs);
+    }
+  };
 
-		// A selection context onclick callback function.
-		$scope.selectionOnClick = function(info){
-			
-			$scope.searchLang = info.menuItemId;
-			$scope.searchTerm = info.selectionText;
-			$scope.search();
-		}
+  // A selection context onclick callback function.
+  $scope.selectionOnClick = function(info) {
 
-		$scope.createContextMenu = function(){
-			var context = "selection";
-			var title = "Search '" + context + "' with Credise";
-			var id = chrome.contextMenus.create({"title": title, "contexts":[context]});
+    $scope.searchLang = info.menuItemId;
+    $scope.searchTerm = info.selectionText;
+    $scope.search();
+  };
 
-			for (var item in $scope.langOptions) {
-					chrome.contextMenus.create({"id":item, "title": $scope.langOptions[item], "parentId": id, "contexts":[context], "onclick": function(info){
-						$scope.selectionOnClick(info)}});
-			}
+  $scope.createContextMenu = function() {
+    var context = "selection";
+    var title = "Search '" + context + "' with Genie Multisearch";
+    var id = chrome.contextMenus.create({
+      "title": title,
+      "contexts": [context]
+    });
 
-			chrome.contextMenus.create({"type":"separator", "parentId": id, "contexts":[context]});
+    
 
-			chrome.contextMenus.create({"parentId": id, "title":"Options", "contexts":[context],"onclick": function(){
-				$scope.openSettings();
-			}});
-	
-		}
+  $scope.langMenu.forEach(function(item){
+    chrome.contextMenus.create({
+      "id": item.code,
+      "title": item.name,
+      "parentId": id,
+      "contexts": [context],
+      "onclick": function(info) {
+        $scope.selectionOnClick(info);
+      }
+    });
+  });
 
-		$scope.openSettings = function(){
-			chrome.tabs.create({'url': "/views/options.html" } )
-		}
+    // for (var item in $scope.langMenu) {
+    //   chrome.contextMenus.create({
+    //     "id": item,
+    //     "title": $scope.langMenu[item],
+    //     "parentId": id,
+    //     "contexts": [context],
+    //     "onclick": function(info) {
+    //       $scope.selectionOnClick(info);
+    //     }
+    //   });
+    // }
 
-		$scope.createContextMenu();
-	}])
+    chrome.contextMenus.create({
+      "type": "separator",
+      "parentId": id,
+      "contexts": [context]
+    });
 
-	.controller('optionsCtrl', ['$scope','$localStorage', '$stateParams', 'Languages', function($scope, $localStorage, $stateParams, Languages) {
+    chrome.contextMenus.create({
+      "parentId": id,
+      "title": "Options",
+      "contexts": [context],
+      "onclick": function() {
+        $scope.openSettings();
+      }
+    });
+  };
 
-		$scope.loadDefaults = function(){			
-			Languages.loadDefaults()
-		}
+  $scope.openSettings = function() {
+    chrome.tabs.create({
+      'url': "/views/options.html"
+    });
+  };
 
-	  	$scope.newtab = {};
-	  	$scope.editmode = false;
-	  	$scope.nativeLang = Languages.native();
-	  	$scope.nativeLangs = Languages.getNativeLangs();
-	  	$scope.availableLangs = Languages.options();
+  $scope.createContextMenu();
+}])
 
-	  	$scope.setNativeLang = function (argument) {
-		  	$localStorage.nativeLang = argument || 'eng';
-		  	$scope.nativeLang = argument;
-		  	$scope.availableLangs = Languages.options();
-		  	console.log($scope.availableLangs);
-	  	}
+.controller('modalCtrl', ['$scope', '$uibModalInstance', 'Languages', function($scope, $uibModalInstance, Languages) {
+    $scope.ok = function() {
+      Languages.loadDefaults();
+      $uibModalInstance.close();
+    };
 
-	  	var id = $stateParams.id;
-	  	// $scope.setLangs("English");
-	  	$scope.save = function(form) {
-	  		$scope.submitted = true;
-	  	
-	  	if(form.$valid){
-	  		if(!$scope.editmode){
-	  			var newtab = $localStorage[id] || [];
-	  			$scope.newtab.id = newtab.length;
-	  			newtab.push($scope.newtab);
+    $scope.cancel = function() {
+      $uibModalInstance.dismiss('cancel');
+    };
+  }])
+  .controller('optionsCtrl', ['$log', '$scope', '$state', '$localStorage', '$stateParams', '$uibModal', 'Languages', function($log, $scope, $state, $localStorage, $stateParams, $uibModal, Languages) {
+    $scope.loadDefaults = function() {
+      var id = $stateParams.id;
 
-	  			$localStorage[id] = newtab;
-	  		} else {
-	  			var newtab = $localStorage[id] || [];
-	  			newtab.forEach(function(tab){
-	  				if (tab.id == $scope.newtab.id) {
-	  					tab = $scope.newtab;
-	  				}
-	  			});
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: './modal.html',
+        controller: 'modalCtrl',
+        size: 'md',
+      });
 
-	  			$localStorage[id] = newtab;
-	  		}
-	  		$scope.editmode = false;
-	  		$scope.newtab = {};
-	  		$scope.form.$setPristine(); 
-	  	}
-	  		$scope.load();		
-	  	}
-	   
-		$scope.load = function() {
-			var id = id || $stateParams.id; 
-			alert(id)
-			$scope.tabs = $localStorage[id];
-			console.log($scope.tabs)
-		}
-
-	  
-		$scope.delete = function(tab){
-			var items = $localStorage[id];
-			var items = _.without(items, tab);
-
-			$localStorage[id] = items;
-			$scope.tabs = $localStorage[id];
-		}
-
-		$scope.edit = function(tab){
-			$scope.newtab = tab
-			$scope.editmode = true
-			console.log($scope.newtab)
-		}
-
-		$scope.tabs = $localStorage[id];
-
-		$scope.travailability = true
-	}])
-
-	.filter('checkmark', function() {
-	  return function(input) {
-	    return input ? '\u2713' : '\u2718';
-	  };
-	})
-
-	.config(function ($stateProvider) {
-		$stateProvider
-		.state('details', {      	
-		url: '/{id}',
-		templateUrl: '../views/eng.html',
-		controller: 'optionsCtrl'
-		})
-	})
-
-	.run(function() {
-		chrome.runtime.onInstalled.addListener(function (details) {
-			console.log('previousVersion', details.previousVersion);
-		});
-
-		chrome.browserAction.setBadgeText({text: 'GNIE'});
-		
-	});
+      modalInstance.result.then(function() {
+        $state.go($state.current, {
+          id: 'engdeu'
+        });
+      }, function() {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
 
 
-	// Vanila JS Functions Goes Here
+      // Languages.loadDefaults();
 
-	function createTabs (alltabs){
-		var i = 0;
-		alltabs.forEach(function(taburl){
+    };
+    $scope.newtab = {};
+    $scope.editmode = false;
+    $scope.nativeLang = Languages.myNative();
+    $scope.nativeMenu = Languages.nativeMenu();
+    $scope.langMenu = Languages.langMenu('options');
 
-			chrome.tabs.create({
-				index: i,
-				url: taburl
-			}, function(tab){
-				console.log(taburl);
-				i++;
-			})
-		})
+    $scope.setNativeLang = function(argument) {
+      $localStorage.nativeLang = argument || 'eng';
+      $scope.nativeLang = argument;
+      $scope.langMenu = Languages.langMenu();
+    };
 
-	}
+    $scope.currentState = $stateParams.id;
+    var id = $stateParams.id;
+
+    $scope.isTrAvailable = function() {
+      if ($stateParams.id !== 'engeng') {
+        return true;
+      }
+    };
+
+  }])
+
+.controller('tabsCtrl', ['$scope', '$timeout', '$localStorage', '$stateParams', 'Languages', 'tabs', 'mStatus',function($scope, $timeout, $localStorage, $stateParams, Languages, tabs, mStatus) {
+  var id = $stateParams.id;
+  $scope.tabs = tabs;
+  $scope.mStatus = mStatus[0];
+  $scope.editmode = false;
+  $scope.saveBtnTxt = 'Save';
+  $scope.newtab = {};
+  $scope.translator = $localStorage[id].translator;
+  $scope.edit = function(tab) {
+    $scope.newtab = tab;
+    $scope.editmode = true;
+  };
+
+  $scope.save = function(form) {
+    $scope.submitted = true;
+    var id = $stateParams.id;
+    if (form.$valid) {
+      if (!$scope.editmode) {
+        var newtab = $localStorage[id].links || [];
+        $scope.newtab.id = newtab.length;
+        newtab.push($scope.newtab);
+        $localStorage[id].links = newtab;
+      } else {
+        var newtab = $localStorage[id].links || [];
+        newtab.forEach(function(tab) {
+          if (tab.id == $scope.newtab.id) {
+            tab = $scope.newtab;
+          }
+        });
+        // $localStorage[id].links = newtab;
+      }
+      $scope.editmode = false;
+      $scope.newtab = {};
+      $scope.form.$setPristine();
+    }
+    $scope.load();
+  };
+
+  $scope.adSave = function(form) {
+    var id = $stateParams.id;
+    var nativeLang = $stateParams.nativeLang || 'eng';
+    var currentLang = id.substr(3);
+    $scope.saveBtnTxt = 'Saving changes..';
+    var newArr = $localStorage.LangStack[nativeLang].map(function(item){
+      if(item.code === currentLang){
+        item.showInMenu = $scope.mStatus.showInMenu;
+      }
+      return item;
+    });
+    $localStorage.LangStack[nativeLang] = newArr;
+    $scope.saveBtnTxt = 'Saved';
+    
+  };
+
+  $scope.load = function() {
+    var id = $stateParams.id;
+    $scope.tabs = $localStorage[id].links;
+    console.log("THE TAABS", $scope.tabs)
+  };
+
+  $scope.delete = function(tab) {
+    var id = $stateParams.id;
+    var items = $localStorage[id].links;
+    var items = _.without(items, tab);
+
+    $localStorage[id].links = items;
+    $scope.tabs = $localStorage[id].links;
+  };
+}])
+
+.filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+})
+
+.config(function($stateProvider) {
+  $stateProvider
+    .state('details', {
+      url: '/{id}',
+      templateUrl: '../views/eng.html',
+      controller: 'tabsCtrl',
+      resolve: {
+        tabs: function($localStorage, $stateParams) {
+          var id = $stateParams.id;
+          if (typeof $localStorage[id] !== 'undefined') {
+            return $localStorage[id].links;
+          } else {
+            return {};
+          }
+        },
+        mStatus: function($localStorage, $stateParams) {
+          var id = $stateParams.id;
+          var nativeLang = id.substr(0,3);
+          var currentLang = id.substr(3);
+          return $localStorage.LangStack[nativeLang].filter(function(obj) {
+            return obj.code == currentLang;
+          });
+        }
+      }
+    });
+})
+
+.run(function($state) {
+  chrome.runtime.onInstalled.addListener(function(details) {
+    console.log('previousVersion', details.previousVersion);
+  });
+
+  chrome.browserAction.setBadgeText({
+    text: 'GNIE'
+  });
+
+  $state.go("details");
+
+});
+
+
+// Vanila JS Functions Goes Here
+
+function createTabs(alltabs) {
+  console.log("Creating tabs.....");
+  var i = 0;
+  alltabs.forEach(function(taburl) {
+
+    chrome.tabs.create({
+      index: i,
+      url: taburl
+    }, function(tab) {
+      console.log(taburl);
+      i++;
+    });
+  });
+
+}
--- scripts/languages.factory.js
+++ scripts/languages.factory.js
@@ -1,72 +1,185 @@
-'use strict';
+(function() {
+  'use strict';
+  angular
+    .module('credise')
+    .factory('Languages', factory);
+  factory.$inject = ['$localStorage','$http'];
+  /* @ngInject */
+  function factory($localStorage, $http) {
+    var Natives = {
+      'eng': 'English',
+      'deu': 'Deutsch',
+      'rus': 'Russian',
+      'rum': 'Romanian (Română)'
+    };
+    var LangStack = {
+      "eng": [
+        {
+          "code":"ara",
+          "name":"Arabic (الْعَرَبيّة)",
+          "showInMenu": true
+        },
+        {
+          "code":"nld",
+          "name":"Dutch (Nederlands)",
+          "showInMenu": true
+        },
+        {
+          "code":"eng",
+          "name":"English",
+          "showInMenu": true
+        },
+        {
+          "code":"fre",
+          "name":"French (Français)",
+          "showInMenu": true
+        },
+        {
+          "code":"deu",
+          "name":"German (Deutsch)",
+          "showInMenu": true
+        },
+        {
+          "code":"heb",
+          "name":"Hebrew",
+          "showInMenu": true
+        },
+        {
+          "code":"hin",
+          "name":"Hindi (हिन्दी)",
+          "showInMenu": false
+        },
+        {
+          "code":"ita",
+          "name":"Italian (italiano)",
+          "showInMenu": true
+        },
+        {
+          "code":"jpn",
+          "name":"Japanese (日本語)",
+          "showInMenu": true
+        },
+        {
+          "code":"kor",
+          "name":"Korean",
+          "showInMenu": true
+        },
+        {
+          "code":"zho",
+          "name":"Mandarin (中文)",
+          "showInMenu": true
+        },
+        {
+          "code":"pol",
+          "name":"Polish (Polski)",
+          "showInMenu": true
+        },
+        {
+          "code":"por",
+          "name":"Portuguese (português)",
+          "showInMenu": true
+        },
+        {
+          "code":"rum",
+          "name":"Romanian (Română)",
+          "showInMenu": true
+        },
+        {
+          "code":"spa",
+          "name":"Spanish (Español)",
+          "showInMenu": true
+        },
+        {
+          "code":"rus",
+          "name":"Russian",
+          "showInMenu": true
+        }
+      ],
+      "deu": [
+        {
+          "code":"eng",
+          "name":"English",
+          "showInMenu": true
+        }
+      ],
+      "rum": [
+        {
+          "code":"eng",
+          "name":"English",
+          "showInMenu": true
+        }
+      ],
+      "rus": [
+        {
+          "code":"eng",
+          "name":"English",
+          "showInMenu": true
+        }
+      ]
+    };
 
-angular.module('credise')
-	.factory('Languages',function($localStorage, $http){
-		var defaultNative = 'eng';
-		var nativeLangStack = {'eng':'English','deu':'Deutsch','rum':'Română'};
-		var langStack = {
-			"eng":  {	
-						"ara":"Arabic (الْعَرَبيّة)",
-						"eng":"English",
-						"fre":"French (Français)",
-						"deu":"German (Deutsch)",						
-						"heb":"Hebrew",
-						"hin":"Hindi (हिन्दी)",
-						"ita":"Italian (italiano)",	
-						"jpn":"Japanese (日本語)",
-						"kor":"Korean",	
-						"chi":"Mandarin (中文)",	
-						"pol":"Polish (Polski)",
-						"por":"Portuguese (português)",					
-		  				"rum":"Romanian (Română)",	
-		  				"spa":"Spanish (Español)"
-			  		},
-			"deu": 	{	"eng":"English" },
-			"rum": 	{	"eng":"English" }
-		};
+    var service = {
+      langMenu: langMenu,
+      loadDefaults: loadDefaults,
+      nativeMenu: nativeMenu,
+      myNative: myNative,
+      searchLang: searchLang,
+      getTabs: getTabs,
+      getTranslator: getTranslator
+    };
+    return service;
+    ////////////////
+    function langMenu(arg) {
+      arg = typeof arg !== 'undefined' ?  arg : 'menu';
+      var nativeCode = $localStorage.nativeLang || 'eng';
+      if($localStorage.hasOwnProperty('LangStack')){
+        var asd = $localStorage['LangStack'][nativeCode];
+        if(arg !== 'menu'){
+          return asd;
+        }
+        return asd
+          .filter(function(item) {
+            console.log(item.showInMenu);
+            return (item.showInMenu == true || item.showInMenu == 'true');
+          });
+      }
+    }
 
-		var translators = {
-			"deu": "http://translate.google.com/translate?hl=en&sl=de&tl=en&u=[translate]",
-			"fre": "http://translate.google.com/translate?hl=en&sl=fr&tl=en&u=[translate]"
-			// "fre": "http://translate.google.com/translate?sl=fr&tl=en&hl=en&ie=UTF-8&u=[translate]"
-		};
+    function loadDefaults(id) {
+      $http.get('http://jasnan.com/lang-resources.json')
+        .then(function(items) {
+          $localStorage['LangStack'] = LangStack;
+          items.data.forEach(function(lang) {
+            var shortcode = 'eng' + lang.shortcode;
+            $localStorage[shortcode] = {};
+            $localStorage[shortcode].translator = lang.translator;
+            $localStorage[shortcode].links = lang.resources[0][shortcode];
+          });
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
 
-		return {
-			native: function(){
-				var native = $localStorage.nativeLang || defaultNative;
-				return native;
-			},
-			options: function(){
-				var native = $localStorage.nativeLang || defaultNative;
-				return langStack[native];
-			},
-			getNativeLangs: function(){
-				return nativeLangStack;
-			},
-			searchLang: function(){
-				return $localStorage.searchLang || "eng";
-			},
-			getTabs: function(id){
-				return $localStorage[id]
-			},
-			getTranslator: function(lang){
-				return translators[lang];
-			},
-			loadDefaults: function(id){
+    function nativeMenu() {
+      return Natives;
+    }
 
-				$http.get('../assets/js/defaults.json')
-				.success(function(res){
-					// alert(JSON.stringify(res))
-					for (var key in res) {
-					  if (res.hasOwnProperty(key)) {
-					  	$localStorage[key] = res[key];
-					    // alert(key + " -> " + res[key]);
-					  }
-					}					
-				})
-				.error(function(err){
-					console.log(err)
-				})
-			}
-		}
-	})
+    function myNative() {
+      return $localStorage.nativeLang || 'eng';;
+    }
+
+    function searchLang() {
+      return $localStorage.searchLang || "eng";
+    }
+
+    function getTabs(id) {
+    	console.log("The tabs")
+      return $localStorage[id].links;
+    }
+
+    function getTranslator(lang) {
+        return $localStorage[lang].translator;
+    }
+  }
+})();
